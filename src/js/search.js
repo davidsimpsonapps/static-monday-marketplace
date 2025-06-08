86ad924cc77dedc8a1d3bf67e4bcf4e1be88fce0
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2025-06-08 13:05:40 +0100

    Update count bubble on search

--- src/js/search.js
+++ src/js/search.js
@@ -6,12 +6,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchText = e.target.value.toLowerCase();
     const appCards = document.querySelectorAll('.card');
     
-    appCards.forEach(card => {
+    for (const card of appCards) {
       if (card.innerText.toLowerCase().includes(searchText)) {
         card.style.display = 'block';
       } else {
         card.style.display = 'none';
       }
-    });
+    }
+    // appCards.forEach(card => {
+    //   if (card.innerText.toLowerCase().includes(searchText)) {
+    //     card.style.display = 'block';
+    //   } else {
+    //     card.style.display = 'none';
+    //   }
+    // });
+
+    const visbleApps = [...appCards].filter(card => card.style.display === 'block');
+
+    const totalsBadge = document.querySelector('[data-total]');
+    if (totalsBadge) {
+      if (visbleApps.length !== parseInt(totalsBadge.getAttribute('data-total'))) {
+        totalsBadge.innerHTML = `${visbleApps.length} / ${totalsBadge.getAttribute('data-total')}`;
+      } else {
+        totalsBadge.innerHTML = totalsBadge.getAttribute('data-total');
+      }
+    }
+
   });
 }); 
