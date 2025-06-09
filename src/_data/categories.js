5c1f82ae8adc90674478369f2b6704aeaf45c616
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2025-06-09 17:17:13 +0100

    Set categories correctly. Also this commit should enable Cloudflare web analytics 🤞

--- src/_data/categories.js
+++ src/_data/categories.js
@@ -6,11 +6,14 @@ module.exports = async function() {
     const data = await response.json();
     
     // Filter out categories that shouldn't be displayed
-    return data.marketplace_categories.filter(category => 
-      category.display && 
-      category.type === 'standard' && 
-      category.app_type === 'app'
-    );
+    return data.marketplace_categories
+      .filter(category => category.display && category.showInLeftPane  )
+      // .filter(category => 
+      //   category.display && 
+      //   category.type === 'standard' && 
+      //   category.app_type === 'app'
+      // )
+      .filter(category => ![12, 15, 10000017, 10000016, 10000004].includes(category.id));
   } catch (error) {
     console.error('Error fetching categories:', error);
     return [];
