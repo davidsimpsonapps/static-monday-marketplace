05322b8ec815f9ae0e960e8065ac6f1e130b18d5
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2025-06-17 14:12:29 +0100

    Vendor blocklist

--- src/_data/marketplace.js
+++ src/_data/marketplace.js
@@ -1,13 +1,11 @@
 const fetch = require('node-fetch');
 
+const { vendorBlockList } = require('./data-filters');
+
 module.exports = async function() {
   try {
     const response = await fetch('https://cdn.monday.com/public_marketplace_apps');
     const data = await response.json();
-    const vendorBlockList = [
-      1, // monday.com
-      10000022 // Test Developer
-    ];
     return data.marketplace_apps.filter(app => !vendorBlockList.includes(app.marketplace_developer_id));
   } catch (error) {
     console.error('Error fetching marketplace apps:', error);
