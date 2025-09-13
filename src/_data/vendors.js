555a6b34ad4d4d0a59028e5cb9c66c0798133924
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2025-09-13 14:29:11 +0100

    Fix to remove vendors with no app installs from sitemap & prevent linking to their vendor page

--- src/_data/vendors.js
+++ src/_data/vendors.js
@@ -6,7 +6,8 @@ module.exports = async function() {
   try {
     const response = await fetch('https://marketplace-ms.monday.com/marketplace_ms/public/marketplace-developers?includeEnrichment=true');
     const data = await response.json();
-    return data.marketplace_developers.filter(app => !vendorBlockList.includes(app.id));
+    // added vendor.installs, so that vendors that have no apps that are not installed are not shown
+    return data.marketplace_developers.filter(vendor => !vendorBlockList.includes(vendor.id) && vendor.installs);
   } catch (error) {
     console.error('Error fetching marketplace vendors:', error);
     return [];
