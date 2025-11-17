e4b127067d909c8239a3f516e4589db39eee0cea
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2025-11-17 22:22:22 +0000

    Remove some test apps from mdc developers

--- src/_data/categories.js
+++ src/_data/categories.js
@@ -1,27 +1,35 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-module.exports = async function() {
+module.exports = async function () {
   try {
-    const response = await fetch('https://marketplace-ms.monday.com/marketplace_ms/public/marketplace-categories');
+    const response = await fetch(
+      "https://marketplace-ms.monday.com/marketplace_ms/public/marketplace-categories"
+    );
     const data = await response.json();
-    
+
     // Filter out categories that shouldn't be displayed
-    return data.marketplace_categories
-      .filter(category => category.display && category.showInLeftPane  )
-      // .filter(category => 
-      //   category.display && 
-      //   category.type === 'standard' && 
-      //   category.app_type === 'app'
-      // )
-      .filter(category => ![
-        12, // Views by monday
-        15, // New apps - not correctly populated
-        10000017, // Recently viewed - always empty
-        10000016, // Favorites - always empty 
-        // 10000004, // Trending this week - 🤷‍♂️ Always zero
-      ].includes(category.id));
+    return (
+      data.marketplace_categories
+        .filter((category) => category.display && category.showInLeftPane)
+        // .filter(category =>
+        //   category.display &&
+        //   category.type === 'standard' &&
+        //   category.app_type === 'app'
+        // )
+        .filter(
+          (category) =>
+            ![
+              12, // Views by monday
+              15, // New apps - not correctly populated
+              10000017, // Recently viewed - always empty
+              10000016, // Favorites - always empty
+              10000020, // For you
+              // 10000004, // Trending this week - 🤷‍♂️ Always zero
+            ].includes(category.id)
+        )
+    );
   } catch (error) {
-    console.error('Error fetching categories:', error);
+    console.error("Error fetching categories:", error);
     return [];
   }
-}; 
+};
--- src/_data/data-filters.js
+++ src/_data/data-filters.js
@@ -2,6 +2,8 @@ module.exports = {
   vendorBlockList: [
     1, // monday.com
     10000022, // Test Developer
-    100000004 // A test marketplace developer name
-  ]
+    100000004, // A test marketplace developer name
+    10000092, // monday AGP
+    100000109, // Sampark Inc
+  ],
 };
