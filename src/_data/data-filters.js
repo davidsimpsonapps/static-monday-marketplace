2648a988b868004b1d5edf8a47af243e86a63ee7
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2026-01-13 22:11:05 +0000

    remove Rami LTD.

--- src/_data/data-filters.js
+++ src/_data/data-filters.js
@@ -5,5 +5,6 @@ module.exports = {
     100000004, // A test marketplace developer name
     10000092, // monday AGP
     100000109, // Sampark Inc
+    10000072, // Rami LTD. - 6 installs, since December 11, 2022
   ],
 };
