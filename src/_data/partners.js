6c20874bbf92329a4ff13e86d4979baa77289bab
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2026-01-14 10:56:54 +0000

    Add individual partner pages

--- src/_data/partners.js
+++ src/_data/partners.js
@@ -7,7 +7,22 @@ module.exports = async function () {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    return data.partners;
+    const partners = data.partners;
+
+    partners.forEach((p) => {
+      ["profilePictureUrl", "logoUrl"].forEach((i) => {
+        if (p[i].startsWith("assets")) {
+          p[i] = `https://partners.mondayprograms.com/${p[i]}`;
+        }
+        p.employees.forEach((e) => {
+          if (e.imageUrl.startsWith("assets")) {
+            e.imageUrl = `https://partners.mondayprograms.com/${e.imageUrl}`;
+          }
+        });
+      });
+    });
+
+    return partners;
   } catch (error) {
     console.error("Error fetching monday.com partners:", error);
     return [];
