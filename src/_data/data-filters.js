9488a3feffa19c88016812cde9616abc9197de70
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2026-02-24 20:16:39 +0000

    removal of mdc people's apps

--- src/_data/data-filters.js
+++ src/_data/data-filters.js
@@ -6,5 +6,7 @@ module.exports = {
     10000092, // monday AGP
     100000109, // Sampark Inc
     10000072, // Rami LTD. - 6 installs, since December 11, 2022
+    10000985, // Grzegorz Swatowski (from monday.com)
+    10000214, // Grzegorz Swatowski again
   ],
 };
