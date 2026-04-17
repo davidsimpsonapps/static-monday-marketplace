6fb784561224777cb512fe3b9b82b978b591c417
dvdsmpsn-mba-15-m3 <david@davidsimpson.me>
2026-04-17 11:20:37 +0100

    Add theme colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,njk}"
-  ],
+  content: ["./src/**/*.{html,js,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-dark-blue": "#0D4BD0",
+        "brand-mid-blue": "#2A7EFF",
+        "brand-light-blue": "#78ADFE",
+        "brand-white": "#fff",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
